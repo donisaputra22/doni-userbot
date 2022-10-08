943073fb04be89edbc2f97a9862e8a5c9902091e
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-10-08 08:32:55 +0700

    Update gcast.py

--- AyiinXd/modules/gcast.py
+++ AyiinXd/modules/gcast.py
@@ -45,6 +45,7 @@ GCAST_BLACKLIST = [
     -1001347414136,  # ArunaMutualan
     -1001726206158,  # Nandesupport
     -1001718757023,  # Spam PandeUserbot
+    -1001706140729,  #dony support
 ]
 
 Heroku = heroku3.from_key(HEROKU_API_KEY)
